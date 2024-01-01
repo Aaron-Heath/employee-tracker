fe9c4b3f974de119c6ea01e8575ceb53036de097
aheath <aaron.heath5447@gmail.com>
2024-01-01 17:08:23 -0500

    Refactor table views. Logic lives in individual methods

--- EmployeeApp.js
+++ EmployeeApp.js
@@ -13,6 +13,7 @@ class EmployeeApp {
             'id', 'First Name', 'Last Name', 'Role', 'Department', "Manager"
         ],
         'departments': [
+            'id', 'Name'
 
         ]
     };
@@ -43,6 +44,8 @@ class EmployeeApp {
                 }
             ]);
 
+
+
             console.log(response);
             let queryResults;
             let headers;
@@ -56,30 +59,13 @@ class EmployeeApp {
                     return;
 
                 case "View All Employees":
-                    // Query data from database
-                    queryResults = await this.#employeesDao.fetchAllEmployees();
-
-                    // Pull needed data
-                    data = queryResults[0];
-
-                    // Format into rows
-                    rows = data.map(row => Object.values(row));
-
-                    // set headers
-                    headers = this.#tableHeaders['employees'];
-
-                    // Add headers to table
-                    rows.unshift(headers);
-
-                    // Render table
-                    this.#render(rows);
-
-
+                    this.renderEmployees();
                     // Re-prompt main menu prompt
                     this.#menus.main();
                     break;
                 
                 case "View All Roles":
+                    // TODO move to renderRoles
                     queryResults = await this.#employeesDao.fetchAllRoles();
                     data = queryResults[0];
                     console.log(data);
@@ -95,33 +81,170 @@ class EmployeeApp {
                     break;
 
                 case "View All Departments":
+                    // TODO move to renderDepartments
                     //WHEN I choose to view all departments
                     //THEN I am presented with a formatted table showing department names and department ids
+                    queryResults = await this.#employeesDao.fetchAllDepartments();
+
+                    data = queryResults[0];
+
+                    rows = data.map(row => Object.values(row));
+                    headers = this.#tableHeaders['departments'];
+
                 
-                    default:
+                    rows.unshift(headers);
+                    this.#render(rows);
+
+                    this.#menus.main();
+                    break;
+
+                default:
                     console.log("Something went wrong, please start again.");
                     return;
             }
 
 
         } ;
+
+        this.#menus['department'] = async () => {
+            const response = await inquirer.prompt([
+                {
+                    name: 'departmentName',
+                    message: 'What is the department name',
+                    type: 'input'
+                }
+            ]);
+
+            await this.#employeesDao.insertDepartment(response.departmentName);
+
+        }
     }
 
     #render(rows) {
         console.log(table(rows));
     }
+    async mainMenu() {
+        const response = await inquirer.prompt([
+            {
+                name:"main",
+                message:"What would you like to do",
+                type: "list",
+                choices: [
+                    "View All Employees",
+                    "View All Roles",
+                    "View All Departments",
+                    "Add A Department",
+                    "Add A Role",
+                    "Add An Employee",
+                    "Update Employee Role",
+                    "Quit"
+                ]
 
+            }
+        ]);
+
+
+
+        // console.log(response);
+        let queryResults;
+        let headers;
+        let rows;
+        let data;
+
+        switch(response.main){
+            
+            case "Quit":
+                console.log("Exiting");
+                return;
+
+            case "View All Employees":
+                await this.renderEmployees();
+                // Re-prompt main menu prompt
+                await this.mainMenu();
+                // this.#menus.main();
+                break;
+            
+            case "View All Roles":
+
+                // Re-prompt main menu prompt
+                await this.renderRoles();
+                await this.mainMenu();
+                
+
+                break;
+
+            case "View All Departments":
+                //WHEN I choose to view all departments
+                //THEN I am presented with a formatted table showing department names and department ids
+                await this.renderDepartments();
+                await this.mainMenu();
+                break;
+
+            default:
+                console.log("Something went wrong, please start again.");
+                return;
+        }
+    }
+    
+    
     async run() {
         await this.init();
-        await this.#menus.main();
+        await this.mainMenu();
 
         return;
         
     }
 
-    async prompt(promptName) {
+    async renderEmployees() {
+        // Query data from database
+        const queryResults = await this.#employeesDao.fetchAllEmployees();
+
+        // Pull needed data
+        const data = queryResults[0];
+
+        // Format into rows
+        const rows = data.map(row => Object.values(row));
+
+        // set headers
+        const headers = this.#tableHeaders['employees'];
+
+        // Add headers to table
+        rows.unshift(headers);
+
+        // Render table
+        await this.#render(rows);
+        
+    }
+
+    async renderDepartments() {
+
+        //WHEN I choose to view all departments
+        //THEN I am presented with a formatted table showing department names and department ids
+        const queryResults = await this.#employeesDao.fetchAllDepartments();
+
+        const data = queryResults[0];
+
+        const rows = data.map(row => Object.values(row));
+        const headers = this.#tableHeaders['departments'];
+
+    
+        rows.unshift(headers);
+        await this.#render(rows);
 
     }
+
+    async renderRoles(){
+        const queryResults = await this.#employeesDao.fetchAllRoles();
+        const data = queryResults[0];
+        console.log(data);
+        const rows = data.map(row => Object.values(row));
+        const headers = this.#tableHeaders['roles'];
+
+    
+        rows.unshift(headers);
+        await this.#render(rows);
+    }
+
 }
 
 module.exports = EmployeeApp;
